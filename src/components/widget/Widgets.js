a25f11ce2141f0377daa94d48d8bc24f53a39628
Kohta9521 <kohta0421kochi@gmail.com>
2023-02-02 15:42:25 +0900

    add plugin

--- src/components/widget/Widgets.js
+++ src/components/widget/Widgets.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import SearchIcon from '@mui/icons-material/Search';
 
+import { TwitterTimelineEmbed, TwitterShareButton, TwitterFollowButton, TwitterHashtagButton, TwitterMentionButton, TwitterTweetEmbed, TwitterMomentShare, TwitterDMButton, TwitterVideoEmbed, TwitterOnAirButton } from 'react-twitter-embed';
+
 function Widgets() {
   return (
     <div className='widgets'>
@@ -17,7 +19,13 @@ function Widgets() {
         <h2>いまどうしてる?</h2>
 
         {/* ライブラリを追加 */}
-        
+        <TwitterTweetEmbed tweetId={"1614977265481437187"} />
+
+        <TwitterTimelineEmbed 
+            sourceType="profile"
+            screenName="kohta_piano"
+            options={{height: 400}}
+        />
       </div>
     </div>
   )
