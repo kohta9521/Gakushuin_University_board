8084a6cfb38473ac9ede1af42a24fc92f6c85a52
Kohta9521 <kohta0421kochi@gmail.com>
2023-02-02 12:33:10 +0900

    add all icons from material ui

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,5 +1,12 @@
 import React from 'react'
 
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import PermIdentityIcon from '@mui/icons-material/PermIdentity';
+import ListAltIcon from '@mui/icons-material/ListAlt';
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import SearchIcon from '@mui/icons-material/Search';
 import HomeIcon from '@mui/icons-material/Home';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import SidebarOption from './SidebarOption';
@@ -12,13 +19,13 @@ const sidebar = () => {
 
         {/* sidebarOprion */}
         <SidebarOption text="ホーム" Icon={HomeIcon}/>
-        <SidebarOption text="話題を検索" Icon={HomeIcon}/>
-        <SidebarOption text="通知" Icon={HomeIcon}/>
-        <SidebarOption text="メッセージ" Icon={HomeIcon}/>
-        <SidebarOption text="ブックマーク" Icon={HomeIcon}/>
-        <SidebarOption text="リスト" Icon={HomeIcon}/>
-        <SidebarOption text="プロフィール" Icon={HomeIcon}/>
-        <SidebarOption text="もっと見る" Icon={HomeIcon}/>
+        <SidebarOption text="話題を検索" Icon={SearchIcon}/>
+        <SidebarOption text="通知" Icon={NotificationsNoneIcon}/>
+        <SidebarOption text="メッセージ" Icon={MailOutlineIcon}/>
+        <SidebarOption text="ブックマーク" Icon={BookmarkBorderIcon}/>
+        <SidebarOption text="リスト" Icon={ListAltIcon}/>
+        <SidebarOption text="プロフィール" Icon={PermIdentityIcon}/>
+        <SidebarOption text="もっと見る" Icon={MoreHorizIcon}/>
 
         {/* ツイートボタン */}
     </div>
