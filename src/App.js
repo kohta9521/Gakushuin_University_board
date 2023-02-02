3606557e6c17d85865f085452f7de6e3bf45a2ba
Kohta9521 <kohta0421kochi@gmail.com>
2023-02-02 13:45:29 +0900

    add timelinejs and timeline css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import Sidebar from "./components/sidebar/sidebar";
+import Timeline from "./components/timeline/timeline";
 
 
 function App() {
   return (
     <div className='app'>
       <Sidebar />
+
+      {/* timeline */}
+      <Timeline />
+
+      {/* widget */}
     </div>
   );
 }
---
+++ src/components/timeline/timeline.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./timeline.css";
+
+function timeline() {
+  return (
+    <div className='timeline'>
+        {/* header */}
+        <div className='timeline__header'>
+            <h2>ホーム</h2>
+        </div>
+        {/* tweetbox */}
+        {/* post */}
+    </div>
+  )
+}
+
+export default timeline
