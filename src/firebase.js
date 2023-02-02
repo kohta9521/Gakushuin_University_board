c799e7b3239a2b1bf1b11e2c90b7ea5fbdb8e5aa
Kohta9521 <kohta0421kochi@gmail.com>
2023-02-02 22:00:37 +0900

    add new firebase files

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCQGGDU4HUXCdsKaBI-ca77WQVBcrKQ3T8",
+  authDomain: "twitter-clone-8ba70.firebaseapp.com",
+  projectId: "twitter-clone-8ba70",
+  storageBucket: "twitter-clone-8ba70.appspot.com",
+  messagingSenderId: "193123878225",
+  appId: "1:193123878225:web:019f84a1ce85028be13f70"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
