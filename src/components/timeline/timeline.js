935f43c8d6ed8c7a636052b011313fd127a9b563
Kohta9521 <kohta0421kochi@gmail.com>
2023-02-04 19:07:26 +0900

    firebaseとの連携完了 エラー解決まで丸2日

--- src/components/timeline/timeline.js
+++ src/components/timeline/timeline.js
@@ -1,16 +1,19 @@
 import React from 'react'
-import "./timeline.css";
+// import react, {useState} from 'react';
+import "./Timeline.css";
 import TweetBox from "./TweetBox";
 import Post from "./Post";
 import db from "../../firebase";
 import { collection, getDocs } from "firebase/firestore"; 
+import react, { useState } from 'react';
 
-function timeline() {
+function Timeline() {
 
-  
+  const [posts, setPosts] = useState([]);
+
   const postData = collection(db, "posts");
   getDocs(postData).then((querySnapshot) => {
-    console.log(querySnapshot.docs.map((doc) => doc.data()));
+    setPosts(querySnapshot.docs.map((doc) => doc.data()));
   });
 
   return (
@@ -21,17 +24,20 @@ function timeline() {
         </div>
         {/* tweetbox */}
         <TweetBox />
+
         {/* post */}
+        {posts.map((post) => (
         <Post 
-          displayName = "Kohta"
-          username = "piano_kohta"
-          varified={true}
-          text = "民法は諦め"
-          avatar=""
-          image = "https://source.unsplash.com/random"
+          displayName = {post.displayName}
+          username = {post.username}
+          varified={post.varified}
+          text = {post.text}
+          avatar={post.avatar}
+          image = {post.image}
         />
+        ))}
     </div>
-  )
+  );
 }
 
-export default timeline;
+export default Timeline;
